81350ffc105390584ef8ab80acc7d5e08f037e81
elahi <elahi@mohammas-mbp.local>
2019-08-23 21:13:50 +0200

    messaged modified

--- src/main/java/com/yellow/product/core/constant/Message.java
+++ src/main/java/com/yellow/product/core/constant/Message.java
@@ -14,7 +14,7 @@ public interface Message {
     public static final String NOT_TEXT_FILE = "The input file should be text file!!";
     public static final String EMPTY_FILE = "The file can not be empty!!";
     public static final String SUCCESSFUL_DOWNLOAD = "The image is successfully stored!!";
-    public static final String FAIL_DOWNLOAD = "The image is failed to fail!!";
+    public static final String FAIL_DOWNLOAD = "No image found to download!!";
     public static final String INVALID_URL = "Invalid url!!";
     public static final String NOT_IMAGE_FILE = "This is not a image file!!";
 }
