07a011b3db694febf3423c89f78e9e975fc707e3
elahi <elahi@fazleh2010>
2020-05-27 20:15:33 +0200

    added the index.js for testing

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+var exec = require('child_process').exec, child;
+child = exec('/usr/bin/java -jar /Users/elahi/NetBeansProjects/prepareVersion/original/sparql-http/target/sparql-http-1.6.jar',
+  function (error, stdout, stderr){
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if(error !== null){
+      console.log('exec error: ' + error);
+    }
+});
