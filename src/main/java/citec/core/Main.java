e55d652cc79a2c156c0b41e2da1cd725752c9b3e
elahi <elahi@fazleh2010>
2020-05-31 16:48:36 +0200

    added connection script

--- src/main/java/citec/core/Main.java
+++ src/main/java/citec/core/Main.java
@@ -55,8 +55,9 @@ public class Main implements SparqlEndpoint {
 
          //my terminology
          System.out.println("Adding my terminology!!"+myTermSparqlEndpoint);
-          Termbase myTerminology = new CurlSparqlQuery(myTermSparqlEndpoint, query_writtenRep, myTermTableName).getTermbase();
+          //Termbase myTerminology = new CurlSparqlQuery(myTermSparqlEndpoint, query_writtenRep, myTermTableName).getTermbase();
           //addToDataBase(myTermTableName, myTerminology, mySQLAccess, limitOfTerms);
+          mySQLAccess.deleteTable(myTermTableName);
 
          //Link terminology
          System.out.println("Adding my other terminology!!"+otherTermTableName);
